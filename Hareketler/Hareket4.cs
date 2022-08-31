a5416ae33faa4bbd49e9845760eb475fa19350fc
52ramazanaydin <44252862+52ramazanaydin@users.noreply.github.com>
2022-08-31 21:04:04 +0300

    Add files via upload

---
+++ Hareketler/Hareket4.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hareket4 : MonoBehaviour
+{
+    Rigidbody yercekimi;
+    public int guc;
+    int puan = 0;
+    public Text puangoster;
+   
+    void Start()
+    {
+        yercekimi = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        float yatay = Input.GetAxisRaw("Horizontal");
+        float dikey = Input.GetAxisRaw("Vertical");
+
+        Vector3 vec = new Vector3(yatay, 0, dikey);
+        yercekimi.AddForce(vec * guc);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ball")
+        {
+            puan++;
+
+            Destroy(other.gameObject);
+        }
+        puangoster.text = "Puanınız:" + puan + " / 18";
+    }
+}
