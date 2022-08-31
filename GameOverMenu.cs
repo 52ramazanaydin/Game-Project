c40367b3e8bfd1a294ec354b3692f006094b5465
52ramazanaydin <44252862+52ramazanaydin@users.noreply.github.com>
2022-08-31 20:29:20 +0300

    Add files via upload

---
+++ GameOverMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+    public void Restart_btn()
+    {
+        SceneManager.LoadScene("Levels");
+    }
+
+    public void MainMenu_btn()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Exit_btn()
+    {
+        Application.Quit();
+    }
+}
